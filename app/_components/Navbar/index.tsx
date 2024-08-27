c639ee439207e4bf96f3db51635294d56c427bdc
Mani Velrajan <mani.velrajan.ctr@ihstowers.com>
2024-08-27 12:17:29 +0530

    Added Logo for Desktop view

--- app/_components/Navbar/index.tsx
+++ app/_components/Navbar/index.tsx
@@ -1,8 +1,18 @@
 import { navLinks } from "@/app/_constants/navlinks";
+import Image from "next/image";
 
 function Navbar() {
   return (
     <nav className="fixed top-0 w-full py-4 px-6 lg:p-6 backdrop-blur-3xl z-10 bg-[#121212] bg-opacity-70">
+      <a className="fixed top-0 left-0 hidden lg:block" aria-description="logo" href="#">
+        <Image
+          src={"/BrandLogo/MR_Logo.svg"}
+          width={150}
+          height={80}
+          alt="logo"
+          className="h-full aspect-auto"
+        />
+      </a>
       <ul className="flex gap-6 sm:gap-10 items-center justify-center lg:justify-end lg:mr-20">
         {navLinks.map(({ href, label }) => {
           return (
