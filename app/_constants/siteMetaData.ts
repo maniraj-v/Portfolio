f271d92c37d55a911c624ebf56eeb49e859d4c51
Mani Velrajan <mani.velrajan.ctr@ihstowers.com>
2024-08-28 15:21:12 +0530

    Added meta info

---
+++ app/_constants/siteMetaData.ts
@@ -0,0 +1,16 @@
+export const siteMetaData = {
+  title: "Portfolio | Frontend Engineer",
+  author: "Mani Raj Velrajan",
+  headerTitle: "Next.js Blog",
+  description: "Personal Portfolio showcasing the experience and project works",
+  language: "en-us",
+  theme: "dark", // system, dark or light
+  siteUrl: "https://portfolio-mani-raj-velrajan.vercel.app/", // your website URL
+  siteLogo: "/BrandLogo/MR_Logo.svg",
+  socialBanner: "/projects/Portfolio_Home.png", // add social banner in the public folder
+  email: "vsmaniraj1234@gmail.com",
+  github: "https://github.com/maniraj-v",
+  linkedin: "https://www.linkedin.com/in/mani-raj-velrajan",
+  twitter: "https://x.com/honeymaniraj",
+  locale: "en-US",
+};
