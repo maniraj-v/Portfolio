1cff123bb83a9429c9eeabbb47b197d0bbf15384
Mani Velrajan <mani.velrajan.ctr@ihstowers.com>
2024-08-28 15:54:26 +0530

    Added manifest and favicon

---
+++ manifest.ts
@@ -0,0 +1,21 @@
+import type { MetadataRoute } from "next";
+import { siteMetaData } from "./app/_constants/siteMetaData";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: siteMetaData.appName,
+    short_name: siteMetaData.appName,
+    description: siteMetaData.description,
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+      {
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+    ],
+  };
+}
